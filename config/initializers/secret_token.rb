cc5537af608c310e3f2deb6de4dc69610a59bb9a
Alex Auritt <alexauritt@gmail.com>
2013-02-05 23:09:14 -0500

    initial commit, with ember-rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caligal::Application.config.secret_token = '0500bbbb2a1273c30918db16ee53cbefb61eb167c0ff207473146a4196bd1e2598e1c82e3b992c2701c1ef05f3513a54cbfdbf6493255e6104609a2f2416c160'
